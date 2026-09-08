b176b0fb2491246420e1de98082aa13b8972ef7e
azmourad067-stack <azmourad067@gmail.com>
2026-09-08 14:32:06 +0200

    Delete horse_prono_app directory

--- horse_prono_app/app_core/io_utils.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from io import BytesIO
-
-import pandas as pd
-
-
-def dataframe_to_csv_bytes(df: pd.DataFrame) -> bytes:
-    return df.to_csv(index=False).encode("utf-8-sig")
